7a0037fbfe8e2a1f45b9e845d9d6cbc2cd8baabe
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-10-20 01:30:37 -0500

    [CREATE]

---
+++ 0-bubble_sort.c
@@ -0,0 +1,23 @@
+/*
+* 0-bubble_sort.c - Implement Bubble Sort Algorithm
+* Date: 20, October 2021
+* Author: Oscar Angel and Rodrigo Zárate Algecira
+*/
+
+/**
+* bubble_sort - Implements Bubble sort algorithm
+* @array: pointer
+* @size: structure
+*/
+
+void bubble_sort(int *array, size_t size)
+{
+/* check if next is bigger than actual pointer */
+/* if number is bigger advance pointer positions */
+/* if numer is lower swap positions and advance pointer */
+/* if end is reached */
+/* if swaps were made */
+/* start over again */
+/* else */
+/* end */
+}
