9928954f912b27db4ce58c5779dcac1f2966b099
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-10-21 14:47:41 -0500

    [UPDATE]

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,3 +1,18 @@
+/*
+* 2-selection_sort.c - Implement Selection Sort Algorithm
+* Date: 21, October 2021
+* Author: Oscar Angel and Rodrigo Zárate Algecira
+*/
+
+#include <stddef.h>
+#include "sort.h"
+
+/**
+* selection_sort - Implements Selection sort algorithm
+* @array: pointer
+* @size: structure
+*/
+
 void selection_sort(int *array, size_t size)
 {
 
