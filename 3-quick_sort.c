5988b4cba44fe7760394583f7d6787f46bce9340
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-10-21 14:53:32 -0500

    [UPDATE]

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,3 +1,18 @@
+/*
+* 3-quick_sort.c - Implement Quick Sort Algorithm
+* Date: 21, October 2021
+* Author: Oscar Angel and Rodrigo Zárate Algecira
+*/
+
+#include <stddef.h>
+#include "sort.h"
+
+/**
+* quick_sort - Implements quick sort algorithm
+* @array: pointer
+* @size: structure
+*/
+
 void quick_sort(int *array, size_t size)
 {
 /* do the sort */
