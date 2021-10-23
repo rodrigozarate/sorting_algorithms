c107c76637fd24c78e9ac42bc93f24602ddfbb13
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-10-23 11:52:19 -0500

    [DEBUG]

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -5,12 +5,13 @@
 */
 
 #include <stddef.h>
+#include <stdio.h>
 #include "sort.h"
 
 /**
 * quick_sort - Implements quick sort algorithm
 * @array: pointer
-* @size: structure
+* @size: size_t int special definition
 */
 
 void quick_sort(int *array, size_t size)
@@ -19,34 +20,56 @@ void quick_sort(int *array, size_t size)
 
 	/* setup vars */
 	low = 0;
-	hight = size;
+	high = size - 1;
 	/* Call recursive function */
 	quick_part(array, low, high);
 		
 /* end */
 }
 
+/**
+* quick_part - Implements recursion
+* @array: pointer
+* @low: size_t int special definition
+* @high: size_t int special definition
+*/
 void quick_part(int *array, size_t low, size_t high)
 {
-	size_t init_size;
+	size_t init_size, partition;
 
 	/* setup vars */
+	printf("in quick_part \n");
 	init_size = high;
-	if (array[low] < array[high])
+	printf("low: %zu - high: %zu - init_size: %zu\n", low, high, init_size);
+	printf("prev oop \n");
+	printf("Low: %d - High: %d", array[low], array[high]);
+	if (low < high)
 	{
+		printf("Low: %d - High: %d \n", array[low], array[high]);
 		partition = lomuto(array, low, high, init_size);
 		quick_part(array, low, partition - 1);
 		quick_part(array, partition + 1, high);
 	}
 }
 
-size_t lomuto(int *aray, size_t low, size_t high, size_t size)
+/**
+* lomuto - Implements lomuto partition
+* @array: pointer
+* @low: size_t int special definition
+* @high: size_t int special definition
+* @size: size_t int special definition to be able to print array
+*/
+size_t lomuto(int *array, size_t low, size_t high, size_t size)
 {
 	size_t i, j;
-	int temp;
+	int pivot, temp;
 
+	printf("in lomuto \n");
+	printf("low: %zu - high: %zu - size: %zu\n", low, high, size);
+	pivot = array[high];
 	i = low -1;
-	for(j = low; j < high; j++)
+	j = low;
+	for(; j < high; j++)
 	{
 		if(pivot >= array[j])
 		{
@@ -56,9 +79,11 @@ size_t lomuto(int *aray, size_t low, size_t high, size_t size)
 			array[j] = temp;
 			print_array(array, size);
 		}
+	}
 	temp = array[i + 1];
 	array[i + 1] = array[high];
 	array[high] = temp;
-	}
+	printf("end of for in lomuto \n");
+	print_array(array, size);
 	return (i + 1);
 }
