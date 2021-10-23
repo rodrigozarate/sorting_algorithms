a0989656fd46efb1ed8beecd4daef00fa532fdf9
oangel26 <oa.angel26@gmail.com>
2021-10-23 14:30:12 -0500

    update

--- sort.h
+++ sort.h
@@ -25,5 +25,6 @@ void quick_sort(int *array, size_t size);
 void swap(int a, int b, int *array, size_t size);
 void quick_part(int *array, int low, int high, size_t size);
 size_t lomuto(int *array, int low, int high, size_t size);
+void shell_sort(int *array, size_t size);
 
 #endif /* SORT_H_BIG_O */
