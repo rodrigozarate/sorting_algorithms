11e5ca77a05150067d435124c851bfcac74d8410
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-10-25 20:57:36 -0500

    [UPDATE]

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,4 +1,5 @@
 #include <stddef.h>
+#include <stdio.h>
 #include "sort.h"
 
 /**
@@ -11,75 +12,27 @@
  */
 void shell_sort(int *array, size_t size)
 {
-	size_t i, j, gap;
-	int tmp;
-	int second_swap;
+	size_t i, j, k, gap;
+	int temp;
 
-	if (!array || !size || size < 2)
-		return;
-
-	gap = 1;
-	while (gap < size)
-		gap = gap * 3 + 1;
-	/* esto debe pasar despues */
-	/* gap /= 3; */
-	while (gap != 1)
+	/* create array of gaps lower than array size */
+	for (i = 1, gap = 1; gap < 2; i += 1)
 	{
-		i = 0;
-		j = gap;
-		while (j < size)
+		 gap = gap * 3 + 1;
+	}
+	while (gap > 0)
+	{
+		for (j = gap; j < size; j++)
 		{
-			if (array[i] > array[j])
+			temp = array[j];
+			for (k = j; k >= gap && array[k - gap] > temp; k -= gap)
 			{
-				tmp = array[i];
-				array[i] = array[j];
-				array[j] = tmp;
-				second_swap = i - gap;
-					if (second_swap >= 0 && array[second_swap] > array[i])
-					{
-						tmp = array[second_swap];
-						array[second_swap] = array[i];
-						array[i] = tmp;
-					}
+				array[k] = array[k - gap];
 			}
-			i++;
-			j++;
+			array[k] = temp;
 		}
-		gap /= 3;
+		gap = (gap - 1) / 3;
+		print_array(array, size);
 	}
-	print_array(array, size);
-	insertionsort(array, size);
-	print_array(array, size);
-}
 
-/**
- * insertionsort - sorts array of integers following the insertion sort alg.
- *
- * @array: Array to convert to a doubly linked list
- * @size: Size of the array
- *
- * Return: Void
- */
-void insertionsort(int *array, size_t size)
-{
-	size_t i, j, k, l;
-	int tmp;
-
-	i = 0;
-	j = 1;
-	while (j < size)
-	{
-		k = i;
-		l = j;
-		while (array[k] > array[l] && k >= 0)
-		{
-			tmp = array[k];
-			array[k] = array[l];
-			array[l] = tmp;
-			k--;
-			l--;
-		}
-		i++;
-		j++;
-	}
 }
